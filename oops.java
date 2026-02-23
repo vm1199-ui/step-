322837798cf83ba7ab5f369b8dbe361c92126649
sai <vm1199@srmist.edu.in>
2026-02-23 22:58:56 +0530

    uc5 done

--- oops.java
+++ oops.java
@@ -1,18 +1,49 @@
-public class oopsbanner {
-    public static void main(String[] args) {
-        //uc5
-        String[] lines = {
-            String.join(" ", "  ***  ", "  ***  ", " ***** ", " ***** "),
-            String.join(" ", " *   * ", " *   * ", " *   * ", " *   * "),
-            String.join(" ", " *   * ", " *   * ", " *   * ", " *      "),
-            String.join(" ", " *   * ", " *   * ", " ***** ", " ***** "),
-            String.join(" ", " *   * ", " *   * ", " *      ", "      * "),
-            String.join(" ", " *   * ", " *   * ", " *      ", " *    * "),
-            String.join(" ", "  ***  ", "  ***  ", " *      ", "  ****  ")
-
+public class OOPSBannerApp {
+    // uc6
+    public static String[] getOPattern() {
+        return new String[] {
+                "  ***  ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                " *   * ",
+                "  ***  "
         };
-        for (String line : lines) {
-            System.out.println(line);
+    }
+    public static String[] getPPattern() {
+        return new String[] {
+                " ***** ",
+                " *   * ",
+                " *   * ",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     "
+        };
+    }
+    public static String[] getSPattern() {
+        return new String[] {
+                "  *****",
+                " *     ",
+                " *     ",
+                "  **** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+    public static void main(String[] args) {
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + "  " +
+                    oPattern[i] + "  " +
+                    pPattern[i] + "  " +
+                    sPattern[i]
+            );
         }
     }
 }
